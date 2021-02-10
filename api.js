4f4b8f26dd863995eb4fa3d318974dcb7a5362cc
Rehan959 <rehanshami007@gmail.com>
2021-02-09 23:05:46 -0500

    Api2

--- api.js
+++ api.js
@@ -1,23 +1,18 @@
-function sendRequest() {
-  let xmlObj = new XMLHttpRequest();
-  xmlObj.open("GET", "https://dog.ceo/api/breeds/image/random/20", true);
-  xmlObj.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      console.log("Yay, successful");
-      const div = document.getElementById("info");
-      const imageArray = JSON.parse(this.responseText).message;
-      document.getElementById("status").innerHTML = JSON.parse(
-        this.responseText
-      ).status;
-      imageArray.forEach((image) => {
-        div.innerHTML += `
-                <img src=${image} alt="dog_image"/>
-                `;
-      });
-    } else {
-      console.log("Nay, unsuccessful");
-    }
-  };
-  xmlObj.send();
+const Loading = document.querySelector("span");
+function FetchRequest() {
+  Loading.classList.add("visual");
+  Loading.classList.remove("invisual");
+  fetch("https://dog.ceo/api/breeds/image/random/20")
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      Loading.classList.add("invisual");
+      Loading.classList.remove("visual");
+    })
+    .catch((error) => {
+      console.log(error);
+      Loading.classList.add("invisual");
+      Loading.classList.remove("visual");
+    });
 }
-sendRequest();
+FetchRequest();
