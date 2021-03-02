73896334f601a1de8a6463384e5dea00aff16b4a
Rehan959 <rehanshami007@gmail.com>
2021-03-02 07:50:25 -0500

    Api

--- api.js
+++ api.js
@@ -1,18 +1,23 @@
-const Loading = document.querySelector("span");
-function FetchRequest() {
-  Loading.classList.add("visual");
-  Loading.classList.remove("invisual");
-  fetch("https://dog.ceo/api/breeds/image/random/20")
-    .then((res) => res.json())
-    .then((result) => {
-      console.log(result);
-      Loading.classList.add("invisual");
-      Loading.classList.remove("visual");
-    })
-    .catch((error) => {
-      console.log(error);
-      Loading.classList.add("invisual");
-      Loading.classList.remove("visual");
-    });
+function sendRequest2() {
+  let xmlObj = new XMLHttpRequest();
+  xmlObj.open("GET", "https://dog.ceo/api/breeds/image/random/20", true);
+  xmlObj.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      console.log("Yay, successful");
+      let section = document.querySelector("section");
+      const imageArray = JSON.parse(this.responseText).message;
+      document.getElementById("status").innerHTML = JSON.parse(
+        this.responseText
+      ).status;
+      imageArray.forEach((image) => {
+        section.innerHTML += `
+              <img src=${image} alt="dog_image"/>
+              `;
+      });
+    } else {
+      console.log("Nay, unsuccessful");
+    }
+  };
+  xmlObj.send();
 }
-FetchRequest();
+sendRequest2();
